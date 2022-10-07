14f55dff22544f5cb27f992a2179e0b29deb61b6
Alberto Da Silva Araujo <alberto.araujo@cea.com.br>
2022-10-07 20:51:17 -0300

    Estilizando tela home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,32 @@
 import { useAuth } from '../hooks/useAuth';
 
-import '../styles/auth.css';
+import logoImg from '../assets/images/logo.png';
+import userIcon from '../assets/images/Male-User.svg';
+import expandArrow from '../assets/images/Expand-Arrow.svg';
+
+import '../styles/home.css';
 
 export function Home() {
   const {user} = useAuth();
 
   return (
-    <div >
-      <h1>{user?.name}</h1>
-     <p>pagina inicial</p>
+    <div id='page-home'>
+      <header>
+        <div className='content'>
+          <img id='logo-pront' src={logoImg} alt="pront Saúde" />
+          <div className='menu-logo'>
+            <p>suporte</p>
+            <img src={userIcon} alt="icone de usuário" />
+            <button><img src={expandArrow} alt="" /></button>
+          </div>
+        </div>
+      </header>
+      <main >
+        <div className='main-content'>
+          <button>atendimentos</button>
+          <button>materiais</button>
+        </div>
+      </main>
     </div>
   )
 }
